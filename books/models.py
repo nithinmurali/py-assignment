ca24975baf241cfeecb465cc9660156505e81514
nithinmurali <imnmfotmal@gmail.com>
2019-06-26 20:53:45 +0530

    added books model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+
+class Book(models.Model):
+    name = models.CharField(max_length=1000)
+    isbn = models.CharField(max_length=14)
+    authors = ArrayField(models.CharField(max_length=1000))
+    number_of_pages = models.PositiveIntegerField()
+    publisher = models.CharField(max_length=1000)
+    country = models.CharField(max_length=500)
+    release_date = models.DateField()
