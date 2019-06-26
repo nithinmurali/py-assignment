f1df44e7f56543156378d450847450c3351cda30
nithinmurali <imnmfotmal@gmail.com>
2019-06-26 22:40:07 +0530

    added services outline

---
+++ books/services/core.py
@@ -0,0 +1,21 @@
+from books.models import Book
+
+
+def create_book(name, isbn, authors, number_of_pages, publisher, country, release_date):
+    pass
+
+
+def get_a_book(book_id):
+    pass
+
+
+def get_books():
+    pass
+
+
+def update_book(book_id, kwargs):
+    pass
+
+
+def delete_book(book_id):
+    pass
