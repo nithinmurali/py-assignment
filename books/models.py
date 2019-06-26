106a5cf6d39a176184fbea7dbd16f7647c35d412
nithinmurali <imnmfotmal@gmail.com>
2019-06-26 22:52:47 +0530

    add uuid id to book

--- books/models.py
+++ books/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
+from uuid import uuid4
 
 
 class Book(models.Model):
+    id = models.UUIDField(default=uuid4, primary_key=True)
     name = models.CharField(max_length=1000)
     isbn = models.CharField(max_length=26)
     authors = ArrayField(models.CharField(max_length=1000))
