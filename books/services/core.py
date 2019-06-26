a84421c64193a8888afa855ef26c26ee0f573153
nithinmurali <imnmfotmal@gmail.com>
2019-06-27 00:08:56 +0530

    added book curd services

--- books/services/core.py
+++ books/services/core.py
@@ -1,21 +1,31 @@
 from books.models import Book
+from rest_framework.exceptions import ValidationError
 
 
 def create_book(name, isbn, authors, number_of_pages, publisher, country, release_date):
-    pass
+    if type(authors) is not list:
+        raise ValidationError("authors should be an array")
+    book = Book.objects.create(name=name, isbn=isbn, authors=authors,number_of_pages=number_of_pages,
+                               publisher=publisher, country=country, release_date=release_date)
+    return book
 
 
 def get_a_book(book_id):
-    pass
+    return Book.objects.get(id=book_id)
 
 
 def get_books():
-    pass
+    return Book.objects.all()
 
 
-def update_book(book_id, kwargs):
-    pass
+def update_book(book_id, **kwargs):
+    book = Book.objects.get(id=book_id)
+    for k, v in kwargs.items():
+        setattr(book, k, v)
+    book.save()
+    return book
 
 
 def delete_book(book_id):
-    pass
+    Book.objects.get(id=book_id).delete()
+    return
