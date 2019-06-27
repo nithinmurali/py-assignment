d94adc166d05a040d4e77b62a4f0c045ebde4248
nithinmurali <imnmfotmal@gmail.com>
2019-06-27 21:55:06 +0530

    added external api views

--- books/serializers.py
+++ books/serializers.py
@@ -6,3 +6,4 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = '__all__'
+        read_only_fields = ('id', )
--- external/views.py
+++ external/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from external.services.external import get_external_books_serialized
+from common.responses import data_response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+
+@api_view(['GET'])
+def api_get_external_books(request, format=None):
+    book_name = request.query_params.get('book', '')
+    books = get_external_books_serialized(book_name)
+    return data_response(200, data=books.data)
