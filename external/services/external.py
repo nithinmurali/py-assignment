a226f5d41d260eeb46685b478bf1bef6e8a4b466
nithinmurali <imnmfotmal@gmail.com>
2019-06-27 21:53:35 +0530

    moved external url into settings

--- external/services/external.py
+++ external/services/external.py
@@ -2,18 +2,19 @@ import requests
 from urllib.parse import quote
 from external.serializers import ExternalBookSerializer
 from common.responses import BadInputError, ServerError
+from django.conf import settings
 
 
 def get_external_books(name):
-    response = requests.get('https://www.anapioficeandfire.com/api/books', params={'q': quote(name)})
+    response = requests.get(settings.FIRE_AND_ICE_URL, params={'q': quote(name)})
     if response.status_code == 200:
         return response.json()
     else:
         raise ServerError("Error in API call")
 
 
-def get_external_books_formatted(name):
+def get_external_books_serialized(name):
     response = get_external_books(name)
     serializer = ExternalBookSerializer(data=response, many=True)
     serializer.is_valid(raise_exception=True)
-    return serializer.validated_data
+    return serializer
