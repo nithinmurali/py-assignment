5e0c3806b4662a4312efbe7cb8b08c6ac82272f2
nithinmurali <imnmfotmal@gmail.com>
2019-06-27 20:31:56 +0530

    added extenal formatted call test

--- external/tests/test_services.py
+++ external/tests/test_services.py
@@ -2,7 +2,7 @@ from unittest.mock import Mock, patch
 from django.test import TestCase
 from common.responses import BadInputError, ServerError
 
-from external.services.external import get_external_books
+from external.services.external import get_external_books, get_external_books_formatted
 
 
 class ExternalBooksTestCase(TestCase):
@@ -45,3 +45,38 @@ class ExternalBooksTestCase(TestCase):
 
         with self.assertRaises(ServerError):
             response = get_external_books("A Game of Thrones")
+
+
+class ExternalBooksFormattedTestCase(TestCase):
+
+    def setUp(self):
+        self.mock_get_patcher = patch('external.services.external.get_external_books')
+        self.mock_get = self.mock_get_patcher.start()
+
+    def tearDown(self):
+        self.mock_get_patcher.stop()
+
+    def test_get_books_when_response_is_ok(self):
+        books = [{
+            "url": "https://www.anapioficeandfire.com/api/books/1",
+            "name": "A Game of Thrones",
+            "isbn": "978-0553103540",
+            "authors": [
+                "George R. R. Martin"
+            ],
+            "numberOfPages": 694,
+            "publisher": "Bantam Books",
+            "country": "United States",
+            "mediaType": "Hardcover",
+            "released": "1996-08-01T00:00:00",
+            "characters": [],
+            "povCharacters": []
+        }]
+
+        # self.mock_get.return_value = Mock()
+        self.mock_get.return_value = books
+
+        response = get_external_books_formatted("A Game of Thrones")
+        response_book = response[0]
+        self.assertEqual(response_book["name"], "A Game of Thrones")
+        self.assertEqual(response_book["number_of_pages"], 694)
